a0591a0250cb07dcb84bfb4f5600e76f53f0a361
tomot7 <tomotarou2020@outlook.jp>
2024-02-13 00:51:15 +0900

    ローカルサーバー用スクリプトを更新

--- cors_http_server.py
+++ cors_http_server.py
@@ -4,6 +4,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Cache-Control', 'no-store, must-revalidate')  # 追加
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
